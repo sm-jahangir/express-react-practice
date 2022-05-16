2cf2c5a5b66281e3949e9f8f3cbe5cfde5b7dced
sm-jahangir <samraatjahangir@gmail.com>
2022-05-16 09:47:13 +0600

    Adding steps

---
+++ backend/__server_deploy.js
@@ -0,0 +1,16 @@
+/**
+ * ----------------------------
+ * ONE TIME for your computer
+ * ------------------------------
+ * 1. Create Heroku Account (One time)
+ * 2. verify email
+ * 3. install heroku cli
+ * 4. heroku login
+ *
+ * ---------------------------
+ * For Each project one time
+ * ---------------------------
+ * 1. heroku create
+ *
+ *
+ */
